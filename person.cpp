292c1520a7af11fa6deede93f9fefd7583ac441b
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-02-21 20:20:06 -0800

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+Person::Person(){
+  payRate = 0.0;
+  hoursWorked = 0.0;
+  firstName = " ";
+  lastName = " ";
+}
+
+void Person::setFirstName(string fName){
+  firstName = fName;
+}
+
 string Person::getFirstName() {
     return firstName;
 }
@@ -13,10 +24,6 @@ void Person::setLastName(string lName){
 
 string Person::getLastName() {
     return lastName;
-}
-
-void Person::setFirstName(string fName){
-  firstName = fName;
 }
 
 void Person::setPayRate(float rate){
